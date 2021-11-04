4a9dfaebf945a2973b2acdb5e1e61b7fa21a0fab
zenginerler <yavuzenginerler@gmail.com>
2021-11-04 00:08:36 -0500

    add core data presets

--- flowe/SceneDelegate.swift
+++ flowe/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  flowe
 //
-//  Created by Mehmet Yavuz Zenginerler on 11/3/21.
+//  Created by Mehmet Yavuz Zenginerler on 11/4/21.
 //
 
 import UIKit
@@ -45,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
--- flowe/ViewController.swift
+++ flowe/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  flowe
 //
-//  Created by Mehmet Yavuz Zenginerler on 11/3/21.
+//  Created by Mehmet Yavuz Zenginerler on 11/4/21.
 //
 
 import UIKit
