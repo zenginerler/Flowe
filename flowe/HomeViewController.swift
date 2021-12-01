3184e48120b20d3bbdd8612dada20b0f78686e9e
YixingMa-1 <yixingma.c.3@gmail.com>
2021-12-01 12:43:09 -0600

    pomo done

--- flowe/HomeViewController.swift
+++ flowe/HomeViewController.swift
@@ -14,44 +14,56 @@ class Pomodoro {
 
 class HomeViewController: UIViewController {
     var timer = Timer()
-    var seconds = 10
+    var minutes = 25
     var pomo: Pomodoro?
     var count = 0
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var RemainTimeLabel: UILabel!
     @IBOutlet weak var numPomoLabel: UILabel!
     var work: Bool = true
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func calendarButtonPress(_ sender: Any) {
         guard let url = URL(string: "calshow://") else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
     @IBAction func startButton(_ sender: Any) {
+        RemainTimeLabel.text = "25"
+        statusLabel.text = "working"
         work = true
-        seconds = 10
+        minutes = 25
         timer.invalidate()
         statusLabel.backgroundColor = UIColor .red
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
     }
     
     
     @IBAction func breakButton(_ sender: Any) {
+        RemainTimeLabel.text = "5"
+        statusLabel.text = "relaxing"
         work = false
-        seconds = 5 
+        minutes = 5
         timer.invalidate()
         statusLabel.backgroundColor = UIColor .green
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
     }
     
+    
+    @IBAction func resetButton(_ sender: Any) {
+        count = 0
+        numPomoLabel.text = "\(count)"
+        
+        
+    }
+    
+    
     @objc func timerClass() {
-        seconds -= 1
-        RemainTimeLabel.text = String(seconds)
-        if seconds == 0 {
+        minutes -= 1
+        RemainTimeLabel.text = String(minutes)
+        if minutes == 0 {
             timer.invalidate()
             if work == true {
                 statusLabel.backgroundColor = UIColor .green
