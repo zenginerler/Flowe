89ba8d11f7cfc09e53afdd87e252de08ec81780a
YixingMa-1 <yixingma.c.3@gmail.com>
2021-12-01 11:02:53 -0600

    keep working on pomodoro

--- flowe/HomeViewController.swift
+++ flowe/HomeViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 import CoreData
 import EventKit
+import Foundation
 
+class Timer {
+    var eventT = ""
+    var locationT = ""
+    var remainTimeT = 0
+}
+
+let queue1 = DispatchQueue(label: "myQueue1", qos: .userInitiated)
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var RemainTimeLabel: UILabel!
+    @IBOutlet weak var numPomoLabel: UILabel!
+    var stopp: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     @IBAction func calendarButtonPress(_ sender: Any) {
