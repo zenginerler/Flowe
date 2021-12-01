58ccfeb6b8d8deb196194593b8636838f75754b8
YixingMa-1 <yixingma.c.3@gmail.com>
2021-12-01 12:18:36 -0600

    keep working on pomo

--- flowe/HomeViewController.swift
+++ flowe/HomeViewController.swift
@@ -8,30 +8,76 @@
 import UIKit
 import CoreData
 import EventKit
-import Foundation
-
-class Timer {
-    var eventT = ""
-    var locationT = ""
-    var remainTimeT = 0
+class Pomodoro {
+    var num = 0
 }
 
-let queue1 = DispatchQueue(label: "myQueue1", qos: .userInitiated)
-
 class HomeViewController: UIViewController {
-
+    var timer = Timer()
+    var seconds = 10
+    var pomo: Pomodoro?
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var RemainTimeLabel: UILabel!
     @IBOutlet weak var numPomoLabel: UILabel!
-    var stopp: Bool = false
-    
+    var work: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     @IBAction func calendarButtonPress(_ sender: Any) {
         guard let url = URL(string: "calshow://") else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
+    
+    @IBAction func startButton(_ sender: Any) {
+        seconds = 10
+        timer.invalidate()
+        statusLabel.backgroundColor = UIColor .red
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
+        
+        if seconds == 0 {
+//            statusLabel.backgroundColor = UIColor .green
+//            self.pomo.num += 1
+//            numPomoLabel.text = String(self.pomo.num)
+        }
+        
+    }
+    
+    
+    @IBAction func breakButton(_ sender: Any) {
+        seconds = 5 
+        timer.invalidate()
+        statusLabel.backgroundColor = UIColor .green
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HomeViewController.timerClass), userInfo: nil, repeats: true)
+        
+    
+    }
+    
+    @objc func timerClass() {
+        seconds -= 1
+        RemainTimeLabel.text = String(seconds)
+        if seconds == 0 {
+            timer.invalidate()
+            if work == true {
+                statusLabel.backgroundColor = UIColor .green
+                work = false
+                self.pomo?.num += 1
+                print("pomonum", self.pomo?.num)
+                
+            }
+            else {
+                statusLabel.backgroundColor = UIColor .red
+                work = true
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
