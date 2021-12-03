94aff1736a5f1d9ba316598633c9427aa102fd2d
Hiran Manoharan <hiran.mano@gmail.com>
2021-12-02 18:15:35 -0600

    Workflow table view setup

---
+++ flowe/Models/Tasks.swift
@@ -0,0 +1,15 @@
+//
+//  Tasks.swift
+//  flowe
+//
+//  Created by Hiran Manoharan on 12/2/21.
+//
+
+import SwiftUI
+
+struct dailyTasks: Identifiable {
+    var id: Int
+    
+    var date: String
+    var task: String
+}
--- flowe/Workflow/WorkflowSwiftUIView.swift
+++ flowe/Workflow/WorkflowSwiftUIView.swift
@@ -7,22 +7,42 @@
 
 import SwiftUI
 
-struct WorkflowSwiftUIView: View {
+var myTasks = [dailyTasks(id: 1, date: "date 1", task: "task 1"),
+               dailyTasks(id: 2, date: "date 2", task: "task 2"),
+               dailyTasks(id: 3, date: "date 3", task: "task 3"),
+               dailyTasks(id: 4, date: "date 4", task: "task 4")]
+
+struct ListRow: View {
+    var eachTask: dailyTasks
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(eachTask.date)
+            Text(eachTask.task)
+        }
+    }
+}
+
+struct WorkflowSwiftUIView: View {
+    var taskToDo: [dailyTasks]
+    var body: some View {
+        NavigationView {
+            List(taskToDo){
+                task in ListRow(eachTask: task)
+            }
+        }.navigationBarTitle(Text("Daily Tasks"))
     }
 }
 
 struct WorkflowSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        WorkflowSwiftUIView()
+        WorkflowSwiftUIView(taskToDo: myTasks)
     }
 }
 
 class ChildHostingController: UIHostingController<WorkflowSwiftUIView> {
 
     required init?(coder: NSCoder) {
-        super.init(coder: coder,rootView: WorkflowSwiftUIView());
+        super.init(coder: coder,rootView: WorkflowSwiftUIView(taskToDo: myTasks));
     }
 
     override func viewDidLoad() {
