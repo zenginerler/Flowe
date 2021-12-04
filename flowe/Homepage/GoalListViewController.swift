eb87694da6e407b951b7082d67157e2640950506
YixingMa-1 <yixingma.c.3@gmail.com>
2021-12-04 11:54:14 -0600

    adding title and date to the journal, and delete row is functioning now

--- flowe/Homepage/GoalListViewController.swift
+++ flowe/Homepage/GoalListViewController.swift
@@ -39,34 +39,36 @@ class GoalListViewController: UIViewController, UITableViewDelegate, UITableView
         // put the value in table view from core data.
         let typee = goallist[row].value(forKey: "type")
         let contentt = goallist[row].value(forKey: "content")
-        let goalintable = "\(typee ?? "")\n   \(contentt ?? "")"
+        let titlee = goallist[row].value(forKey: "title")
+        let datee = goallist[row].value(forKey: "date")
+        let goalintable = "\(datee ?? "")\n   \(titlee ?? "")\n   \(typee ?? "")\n   \(contentt ?? "")"
         cell.textLabel?.text = goalintable
         return cell
     }
 
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            goallist.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//            // delete one core data
-//            let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//            let context = appDelegate.persistentContainer.viewContext
-//            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Goals")
-//            var fetchedResults:[NSManagedObject]
-//            do {
-//                try fetchedResults = context.fetch(request) as! [NSManagedObject]
-//                if fetchedResults.count > 0 {
-//                    context.delete(fetchedResults[indexPath.row])
-//                }
-//                try context.save()
-//            } catch {
-//                let nserror = error as NSError
-//                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-//                abort()
-//            }
-//        } else if editingStyle == .insert {
-//        }
-//    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            goallist.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            // delete one core data
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Goals")
+            var fetchedResults:[NSManagedObject]
+            do {
+                try fetchedResults = context.fetch(request) as! [NSManagedObject]
+                if fetchedResults.count > 0 {
+                    context.delete(fetchedResults[indexPath.row])
+                }
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        } else if editingStyle == .insert {
+        }
+    }
     
     func retrievePizza() -> [NSManagedObject] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
