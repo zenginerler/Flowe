163b89b526a09a7c47103223145faf3205729246
Michael Walters <mike855@outlook.com>
2021-12-05 14:58:18 -0600

    workflow VC rearranging

--- flowe/Workflow/WorkflowVC.swift
+++ flowe/Workflow/WorkflowVC.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
+import SwiftUI
 
 class WorkflowVC: UIViewController {
     
-    @IBOutlet weak var theContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,37 +17,55 @@ class WorkflowVC: UIViewController {
 //        addChild(childView)
 //        childView.view.frame = theContainer.bounds
 //        theContainer.addSubview(childView.view)
-        assignbackground()
+        //assignbackground()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        darkModeCheck()
+        //darkModeCheck()
     }
     
-    func assignbackground() {
-          let background = UIImage()
+    let moc = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let swiftUIView = UIHostingController(rootView: SwiftUIView().environment(\.managedObjectContext, moc!))
+        
+        present(swiftUIView, animated: true, completion: nil)
+    }
 
-          var imageView : UIImageView!
-          imageView = UIImageView(frame: view.bounds)
-          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-          imageView.clipsToBounds = true
-          imageView.image = background
-          imageView.center = view.center
-          view.addSubview(imageView)
-          self.view.sendSubviewToBack(imageView)
-      }
-    
-    func darkModeCheck() {
-        // Update the the theme according to user settings
-        if (Variables.appTheme == 1) {
-            overrideUserInterfaceStyle = .light
-            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_light")
-        } else if (Variables.appTheme == 2) {
-            overrideUserInterfaceStyle = .dark
-            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_dark")
-        } else {
-            print("\nTheme ERROR")
+    struct SwiftUIView: View {
+        @Environment(\.managedObjectContext) var moc
+
+        var body: some View {
+            Text("Hello World")
         }
     }
+    
+//    func assignbackground() {
+//          let background = UIImage()
+//
+//          var imageView : UIImageView!
+//          imageView = UIImageView(frame: view.bounds)
+//          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+//          imageView.clipsToBounds = true
+//          imageView.image = background
+//          imageView.center = view.center
+//          view.addSubview(imageView)
+//          self.view.sendSubviewToBack(imageView)
+//      }
+    
+//    func darkModeCheck() {
+//        // Update the the theme according to user settings
+//        if (Variables.appTheme == 1) {
+//            overrideUserInterfaceStyle = .light
+//            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_light")
+//        } else if (Variables.appTheme == 2) {
+//            overrideUserInterfaceStyle = .dark
+//            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_dark")
+//        } else {
+//            print("\nTheme ERROR")
+//        }
+//    }
 }
