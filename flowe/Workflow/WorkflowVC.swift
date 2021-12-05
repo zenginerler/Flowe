3fd98ad120c2daf1f1a431d8b5780da21925a513
zenginerler <yavuzenginerler@gmail.com>
2021-12-05 17:02:02 -0600

    workflow fullscreen fix

--- flowe/Workflow/WorkflowVC.swift
+++ flowe/Workflow/WorkflowVC.swift
@@ -9,29 +9,29 @@ import UIKit
 import SwiftUI
 
 class WorkflowVC: UIViewController {
-    
+
+    let moc = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    let viewHC = UIHostingController(rootView: SwiftUIView())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let childView = UIHostingController(rootView: WorkflowSwiftUIView())
-//        addChild(childView)
-//        childView.view.frame = theContainer.bounds
-//        theContainer.addSubview(childView.view)
-        //assignbackground()
+//        addChild(viewHC)
+//        viewHC.view.frame = view.bounds
+//        view.addSubview(viewHC.view)
         
+        assignbackground()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        //darkModeCheck()
+        darkModeCheck()
     }
     
-    let moc = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
-        
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         let swiftUIView = UIHostingController(rootView: SwiftUIView().environment(\.managedObjectContext, moc!))
-        
+        swiftUIView.modalPresentationStyle = .fullScreen
         present(swiftUIView, animated: true, completion: nil)
     }
 
@@ -97,29 +97,29 @@ class WorkflowVC: UIViewController {
         }
     }
     
-//    func assignbackground() {
-//          let background = UIImage()
-//
-//          var imageView : UIImageView!
-//          imageView = UIImageView(frame: view.bounds)
-//          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-//          imageView.clipsToBounds = true
-//          imageView.image = background
-//          imageView.center = view.center
-//          view.addSubview(imageView)
-//          self.view.sendSubviewToBack(imageView)
-//      }
+    func assignbackground() {
+          let background = UIImage()
+
+          var imageView : UIImageView!
+          imageView = UIImageView(frame: view.bounds)
+          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+          imageView.clipsToBounds = true
+          imageView.image = background
+          imageView.center = view.center
+          view.addSubview(imageView)
+          self.view.sendSubviewToBack(imageView)
+      }
     
-//    func darkModeCheck() {
-//        // Update the the theme according to user settings
-//        if (Variables.appTheme == 1) {
-//            overrideUserInterfaceStyle = .light
-//            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_light")
-//        } else if (Variables.appTheme == 2) {
-//            overrideUserInterfaceStyle = .dark
-//            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_dark")
-//        } else {
-//            print("\nTheme ERROR")
-//        }
-//    }
+    func darkModeCheck() {
+        // Update the the theme according to user settings
+        if (Variables.appTheme == 1) {
+            overrideUserInterfaceStyle = .light
+            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_light")
+        } else if (Variables.appTheme == 2) {
+            overrideUserInterfaceStyle = .dark
+            (view.subviews[0] as! UIImageView).image = UIImage(named: "sharp_dark")
+        } else {
+            print("\nTheme ERROR")
+        }
+    }
 }
