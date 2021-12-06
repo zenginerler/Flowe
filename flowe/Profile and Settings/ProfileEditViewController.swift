fe54e11b06cc958d9ffde4932aba7f43a5ff4991
Hiran Manoharan <hiran.mano@gmail.com>
2021-12-05 21:29:36 -0600

    Assign Background

--- flowe/Profile and Settings/ProfileEditViewController.swift
+++ flowe/Profile and Settings/ProfileEditViewController.swift
@@ -22,12 +22,26 @@ class ProfileEditViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignBackground()
         fillTextFields()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         darkModeCheck()
     }
+    
+    func assignBackground() {
+          let background = UIImage()
+
+          var imageView : UIImageView!
+          imageView = UIImageView(frame: view.bounds)
+          imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+          imageView.clipsToBounds = true
+          imageView.image = background
+          imageView.center = view.center
+          view.addSubview(imageView)
+          self.view.sendSubviewToBack(imageView)
+      }
     
     func fillTextFields() {
         let singleUser = context.object(with: Variables.userID!)
